2d55018ba442717587bbb10a1bf40338bdfeaa12
Javier Rojas <jrojas03.08.98@gmail.com>
2019-07-31 00:17:48 -0400

    5th Rule working properly, no validation on empty fields yet. Still missing 3rd Rule.

--- visual.py
+++ visual.py
@@ -16,26 +16,18 @@ def create_window():
     #Label 1
     lbl = Label(window, text="Seleccione accion.", font=("Arial Bold", 15))
     lbl.grid(column=1, row=0)
-    #Tracer
-    # v = StringVar()
-    # v.trace('w', on_value_change)
+
     #Cbx
     combo = Combobox(window, state="readonly")
     combo['values'] = ("Select", "libros_menosde_siete_dias(Adicional,Dia,Mes,Anho,IdLibro)"
                        ,"categoria_estrellas(Adicional, Porcentaje, Categoria, Estrellas, IdLibro)"
                        ,"usados_varias_categorias(Adicional,IdLibro)"
-                       ,"categoria_autor_nopalabra(Porcentaje, Categoria, Autor, Palabra, IdLibro)")
+                       ,"categoria_autor_nopalabra(Porcentaje, Categoria, Autor, Palabra, IdLibro)"
+                       ,"categoria_estrellas_estemes(Categoria, Estrellas, Mes, Anho, IdLibro)")
     combo.current(0)
     combo.grid(column=1,row=2)
     combo.bind('<<ComboboxSelected>>', lambda event: on_value_change(combo, window))
-    # txt = scrolledtext.ScrolledText(window, width=40, height=10)
-    # txt.grid(column=0, row=3)
-    # btn = Button(window, text="seleccionar opcion", command= lambda: clicked(combo.get(), txt))
-    # btn.grid(column=0,row=1)
-    # txtEntry = Entry(window)
-    # txtEntry.grid(column=0, row=4)
-    # btnEntry = Button(window, text="Agregar asercion", command= lambda: assertar(txtEntry))
-    # btnEntry.grid(column=0,row=5)
+
     window.mainloop()
 
 
@@ -50,6 +42,7 @@ def clicked1st(adicional, dia, mes, anho, id_libro, window):
 
         print(fact)
         selected_option = list(prolog.query(fact))
+        print(selected_option)
 
         txt = scrolledtext.ScrolledText(window, width=40, height=10)
         txt.grid(column=1, row=15)
@@ -77,7 +70,7 @@ def clicked2nd(adicional, porciento_sueldo, categoria, estrellas, id_libro, wind
             txt.insert(INSERT, (key + ' : ' + str(result[key]) + "\n"))
 
 
-def clicked3rd(porciento_sueldo, categoria, autor, palabra, id_libro, window):
+def clicked4th(porciento_sueldo, categoria, autor, palabra, id_libro, window):
     fact = "categoria_autor_nopalabra(" \
             + porciento_sueldo.get() + \
             "," + categoria.get() + \
@@ -86,6 +79,26 @@ def clicked3rd(porciento_sueldo, categoria, autor, palabra, id_libro, window):
             "," + id_libro.get() + \
             ")"
 
+
+    print(fact)
+    selected_option = list(prolog.query(fact))
+
+
+    txt = scrolledtext.ScrolledText(window, width=40, height=10)
+    txt.grid(column=1, row=15)
+    for result in selected_option:
+        for key in result.keys():
+            txt.insert(INSERT, (key + ' : ' + str(result[key]) + "\n"))
+
+
+def clicked5th(categoria, estrellas, mes, anho, id_libro, window):
+    fact = "categoria_estrellas_estemes(" \
+            + categoria.get() + \
+            "," + estrellas.get() + \
+            "," + mes.get() + \
+            "," + anho.get() + \
+            "," + id_libro.get() + \
+            ")"
 
     print(fact)
     selected_option = list(prolog.query(fact))
@@ -131,7 +144,7 @@ def on_value_change(combo,window):
         btn = Button(window, text="Buscar", command= lambda: clicked1st(adicional,dia,mes,anho,id_libro, window))
         btn.grid(column=1,row=13)
 
-    if(combo.get() == "categoria_estrellas(Adicional, Porcentaje, Categoria, Estrellas, IdLibro)"):
+    elif(combo.get() == "categoria_estrellas(Adicional, Porcentaje, Categoria, Estrellas, IdLibro)"):
         cleanWindow(window)
 
         label_adicional = Label(window, text="Sueldo Adicional", anchor='e', width=30)
@@ -161,7 +174,7 @@ def on_value_change(combo,window):
 
         btn = Button(window, text="Buscar", command=lambda: clicked2nd(adicional, porciento_sueldo, categoria, estrellas, id_libro, window))
         btn.grid(column=1, row=13)
-    if(combo.get() == "usados_varias_categorias(Adicional,IdLibro)"):
+    elif(combo.get() == "usados_varias_categorias(Adicional,IdLibro)"):
         cleanWindow(window)
 
         label_adicional = Label(window, text="Sueldo Adicional", anchor='e', width=30)
@@ -174,7 +187,7 @@ def on_value_change(combo,window):
         id_libro = Entry(window)
         id_libro.grid(column=1, row=5)
 
-    if(combo.get() == "categoria_autor_nopalabra(Porcentaje, Categoria, Autor, Palabra, IdLibro)"):
+    elif(combo.get() == "categoria_autor_nopalabra(Porcentaje, Categoria, Autor, Palabra, IdLibro)"):
         cleanWindow(window)
 
         label_porciento_sueldo = Label(window, text="Porciento Sueldo", anchor='e', width=30)
@@ -182,7 +195,7 @@ def on_value_change(combo,window):
         porciento_sueldo = Entry(window)
         porciento_sueldo.grid(column=1, row=3)
 
-#TITULO/NOMBRE AUTOR/PRECIO FOR THE LISTING OF BOOKS
+# PRINT TITULO/NOMBRE AUTOR/PRECIO FOR THE LISTING OF BOOKS
         label_categoria = Label(window, text="Categoria", anchor='e', width=30)
         label_categoria.grid(column=0, row=5, pady=10)
         categoria = Entry(window)
@@ -204,8 +217,39 @@ def on_value_change(combo,window):
         id_libro.grid(column=1, row=11)
 
         btn = Button(window, text="Buscar",
-                     command=lambda: clicked3rd(porciento_sueldo, categoria, autor, palabra, id_libro, window))
+                     command=lambda: clicked4th(porciento_sueldo, categoria, autor, palabra, id_libro, window))
         btn.grid(column=1, row=13)
+
+    elif (combo.get() == "categoria_estrellas_estemes(Categoria, Estrellas, Mes, Anho, IdLibro)"):
+        label_categoria = Label(window, text="Categoria", anchor='e', width=30)
+        label_categoria.grid(column=0, row=3, pady=10)
+        categoria = Entry(window)
+        categoria.grid(column=1, row=3)
+
+        label_estrellas = Label(window, text="Estrellas", anchor='e', width=30)
+        label_estrellas.grid(column=0, row=5, pady=10)
+        estrellas = Entry(window)
+        estrellas.grid(column=1, row=5)
+
+        label_mes = Label(window, text="Mes", anchor='e', width=30)
+        label_mes.grid(column=0, row=7, pady=10)
+        mes = Entry(window)
+        mes.grid(column=1, row=7)
+
+        label_anho = Label(window, text="Año", anchor='e', width=30)
+        label_anho.grid(column=0, row=9, pady=10)
+        anho = Entry(window)
+        anho.grid(column=1, row=9)
+
+        label_id_libro = Label(window, text="Id Libro", anchor='e', width=30)
+        label_id_libro.grid(column=0, row=11, pady=10)
+        id_libro = Entry(window)
+        id_libro.grid(column=1, row=11)
+
+        btn = Button(window, text="Buscar",
+                     command=lambda: clicked5th(categoria, estrellas, mes, anho, id_libro, window))
+        btn.grid(column=1, row=13)
+
 def cleanWindow(window):
     for widget in window.grid_slaves():
         if (int(widget.grid_info()['row'] > 2)):
